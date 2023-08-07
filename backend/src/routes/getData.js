14092a75a8f107e222e0bb7459933d7795d4d655
ADEP-123 <andep210399@gmail.com>
2023-08-07 08:56:55 -0500

    refactor: :fire: reorganizacion de carpeta middlewate, se cambio de gerarquie al sacarla de la carpeta routes a la src

--- backend/src/routes/getData.js
+++ backend/src/routes/getData.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getSucursalController } from "../controllers/getDataController.js";
-import { configGet } from "./middleware/limit.js";
+import { configGet } from "../middleware/limit.js";
 
 const getInitRoute = () => {
     const router = Router()
